aa82597454aa4cd7215f2759a36d70c0c2e7e2c0
UMICELLPHONE <umicellphone414@gmail.com>
2022-12-17 16:23:24 -0500

    Caracteristicas Equipos

--- models/BlogModel.js
+++ models/BlogModel.js
@@ -2,9 +2,25 @@
 import db from "../database/db.js";
 import { DataTypes } from "sequelize";
 
-const BlogModel = db.define("blogs", {
-  title: {type: DataTypes.STRING},
-  content: {type: DataTypes.STRING},
+const BlogModel = db.define("caracteristicasEquipos", {
+  nombre: { type: DataTypes.STRING },
+  version: { type: DataTypes.STRING },
+  display: { type: DataTypes.STRING },
+  cam_princ: { type: DataTypes.STRING },
+  cam_cant: { type: DataTypes.STRING },
+  cam_front: { type: DataTypes.STRING },
+  mem_int: { type: DataTypes.STRING },
+  mem_exp: { type: DataTypes.STRING },
+  nucleos: { type: DataTypes.STRING },
+  ghz: { type: DataTypes.STRING },
+  ram: { type: DataTypes.STRING },
+  peso: { type: DataTypes.STRING },
+  altura: { type: DataTypes.STRING },
+  anchura: { type: DataTypes.STRING },
+  grosor: { type: DataTypes.STRING },
+  bateria: { type: DataTypes.STRING },
+  red: { type: DataTypes.STRING },
+
 })
 
 export default BlogModel;
